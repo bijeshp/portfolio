69a4ecb4a4385f1e51a3a1b2f1dc1dd1fc981508
Bijesh Purushan <bijeshp009@gmail.com>
2019-09-26 14:21:45 -0700

    Bug Fixes

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -1,40 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
-import Particles from 'react-particles-js';
-import HeroBackground from '../images/hero-bg.jpg'
+import Particles from "react-particles-js"
+import HeroBackground from "../images/hero-bg.jpg"
 import { About } from "../data"
 
 class Hero extends React.Component {
   constructor(props) {
     super(props)
   }
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
   render() {
     const { name, quote, social } = About
     return (
-      <section className="panel hero" id="home" style={{backgroundImage:`url(${HeroBackground})`}}>
-      <Particles
-      className='background'
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 50
-	        },
-	        "size": {
-	            "value": 3
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": "repulse"
-	            }
-	        }
-	    }
-	}} />
+      <section
+        className="panel hero"
+        id="home"
+        style={{ backgroundImage: `url(${HeroBackground})` }}
+      >
+        <Particles
+          className="background"
+          params={{
+            particles: {
+              number: {
+                value: 50,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+            },
+          }}
+        />
         <div className="foreground">
           <div className="intro">
             <strong>Hello !</strong>
